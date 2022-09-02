3b9712829520d99e3aaad0a57a78f232eeadb3ad
Abhishek Banerjee <109202077+abhiziko10@users.noreply.github.com>
2022-09-02 14:54:44 +0530

    Update BookInterface.java

--- src/BookDetails/BookInterface.java
+++ src/BookDetails/BookInterface.java
@@ -1,11 +1,13 @@
 package BookDetails;
 
 public interface BookInterface {
-    void addBook();
+    
     void updateBook();
 
     void addBook(Book book);
 
     void showBook();
         void delete();
+     void sortBookByID();
+           void sortBookByPublisher();
 }
